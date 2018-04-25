d2b953365ac6da6fa445f0794864861ad2d20363
dcos <dcos@localhost>
2018-04-26 00:43:05 +0800

    init

---
+++ getLastOne.py
@@ -0,0 +1,36 @@
+# coding=utf-8
+#!/usr/bin/env python
+import sys
+
+
+def getPositionN(n, mark, last, positionNum, loopDepth):
+    removeCount = 0
+    for i in range(n):
+        if positionNum == 2:
+            removeCount += 1
+            positionNum = 0
+        else:
+            mark[i-removeCount] = mark[i]
+            last = mark[i]
+            positionNum += 1
+    left = n-removeCount
+    if left > 1:
+        loopDepth += 1
+        run(left, mark[:left], last, positionNum, loopDepth)
+    else:
+        print "last:{}, depth:{}".format(last, loopDepth)
+
+
+def getStartFrom(n):
+    left=n
+    starts = []
+    while n > 1:
+        pass
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        total = int(sys.argv[1])
+    else:
+        sys.exit('Need param N!')
+    getLastDiv3(total)
