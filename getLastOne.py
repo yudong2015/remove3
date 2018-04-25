f349416b9dc331156e9dd070c9c0a0ebc4b9957d
dcos <dcos@localhost>
2018-04-26 01:21:12 +0800

    init

--- getLastOne.py
+++ getLastOne.py
@@ -3,34 +3,40 @@
 import sys
 
 
-def getPositionN(n, mark, last, positionNum, loopDepth):
-    removeCount = 0
-    for i in range(n):
-        if positionNum == 2:
-            removeCount += 1
-            positionNum = 0
-        else:
-            mark[i-removeCount] = mark[i]
-            last = mark[i]
-            positionNum += 1
-    left = n-removeCount
-    if left > 1:
-        loopDepth += 1
-        run(left, mark[:left], last, positionNum, loopDepth)
-    else:
-        print "last:{}, depth:{}".format(last, loopDepth)
+def getLastOne(n):
+    starts = getStartFroms(n)
+    print starts
+    positionN1 = 1
+    """
+    positionN = (3positionN1 + startN - 1 - a) / 2
+    
+    a = 1 or 2
+    the type of positionN is int
+    """
+    for startN in starts[::-1]:
+        result, remainder = divmod(positionN1*3+startN-1, 2)
+        """
+        a = 1 if remainder else 2
+        positionN = divmod(positionN1*3+startN-1-a, 2)
+        """
+        positionN = result if remainder else result - 1
+        positionN1 = positionN
+    print "The last one is: {}".format(positionN1)
 
 
-def getStartFrom(n):
-    left=n
+def getStartFroms(n):
     starts = []
+    end = 0
     while n > 1:
-        pass
+        starts.append(end+1)
+        removedNum, end = divmod(n+end, 3)
+        n = n - removedNum
+    return starts
 
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        total = int(sys.argv[1])
+        n = int(sys.argv[1])
     else:
         sys.exit('Need param N!')
-    getLastDiv3(total)
+    getLastOne(n)
